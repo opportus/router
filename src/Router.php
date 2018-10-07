f12413d95f648c5630469d429e7449bb188cbb51
opportus <opportus@gmail.com>
2018-10-07 19:03:30 +0300

    Fixes code standards

--- src/Router.php
+++ src/Router.php
@@ -11,49 +11,48 @@ namespace Opportus\Router;
  */
 class Router
 {
-	/**
-	 * @var array $routes
-	 */
-	protected $routes = array();
+    /**
+     * @var array $routes
+     */
+    protected $routes = array();
 
-	/**
-	 * Registers a route.
-	 *
-	 * @param string $regexPath
-	 * @param array  $endpoint  Respect the following structure:
-	 *
-	 * array(
-	 *     'controller' => 'Vendor\Controller',
-	 *     'action'     => 'view'
-	 * )
-	 */
-	public function register(string $regexPath, array $endpoint)
-	{
-		$this->routes[$regexPath] = $endpoint;
-	}
+    /**
+     * Registers a route.
+     *
+     * @param string $regexPath
+     * @param array  $endpoint  Respect the following structure:
+     *
+     * array(
+     *     'controller' => 'Vendor\Controller',
+     *     'action'     => 'view'
+     * )
+     */
+    public function register(string $regexPath, array $endpoint)
+    {
+        $this->routes[$regexPath] = $endpoint;
+    }
 
-	/**
-	 * Resolves the given URI's path based on the registered routes.
-	 * Returns the controller, action and params.
-	 *
-	 * @param  string $uriPath
-	 * @return array  $results
-	 */
-	public function resolve(string $uriPath)
-	{
-		$results = array();
+    /**
+     * Resolves the given URI's path based on the registered routes.
+     * Returns the controller, action and params.
+     *
+     * @param  string $uriPath
+     * @return array  $results
+     */
+    public function resolve(string $uriPath)
+    {
+        $results = array();
 
-		foreach ($this->routes as $regexPath => $endpoint) {
-			if (preg_match($regexPath, $uriPath, $matches)) {
-				$results['controller'] = $endpoint['controller'];
-				$results['action']     = $endpoint['action'];
-				$results['params']     = isset($matches[1]) ? explode('/', trim($matches[1], '/')) : array('');
+        foreach ($this->routes as $regexPath => $endpoint) {
+            if (preg_match($regexPath, $uriPath, $matches)) {
+                $results['controller'] = $endpoint['controller'];
+                $results['action']     = $endpoint['action'];
+                $results['params']     = isset($matches[1]) ? explode('/', trim($matches[1], '/')) : array('');
 
-				break;
-			}
-		}
+                break;
+            }
+        }
 
-		return $results;
-	}
+        return $results;
+    }
 }
-
